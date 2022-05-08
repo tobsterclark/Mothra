bb87d5469a679b24cb51565a9438c9bb597a96e2
tobsterclark <tobsterclark@gmail.com>
2022-05-08 22:12:10 +1000

    Setting up the default structure for mothra

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<span>This is the base for Mothra!</span>
+			</header>
+		</div>
+	);
+}
+
+export default App;
